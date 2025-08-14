9e3011741d2d5f8a5c65e669c1d6139349bed908
Nishant Maheshwari <nishant2622@gmail.com>
2025-08-15 03:20:47 +0530

    add a curse word counter logic for each word

--- curse.js
+++ curse.js
@@ -2,17 +2,29 @@ let textInput = document.getElementById("textInput");
 let button = document.getElementById("submitBtn"); 
 let curseWords = ['cunt','fuck','bitch']
 let curseCounter = 0
+let obj = {
+
+}
 
 button.addEventListener("click", () => {
   let words = textInput.value.toLowerCase().trim().split(/\s+/);
   for(let word of words){
-    curseWords.forEach((curse) => {
-   if(word === curse){
-    curseCounter++
-   }
-    })
+  if(curseWords.includes(word)){
+  storeWords(word)
+  curseCounter++
+  }
     
   }
   console.log(curseCounter);
+  console.log(obj);
   
-})
+  
+}) 
+
+function storeWords(word){
+if(obj[word]){
+  obj[word]++
+}else{
+  obj[word] = 1
+}
+}
