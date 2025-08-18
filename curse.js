df853006618f31f6b28c7dc35297f704f0cf2fc5
Nishant Maheshwari <nishant2622@gmail.com>
2025-08-19 04:01:30 +0530

    feat: show each curse word alongside its count

--- curse.js
+++ curse.js
@@ -3,6 +3,7 @@ let button = document.getElementById("submitBtn");
 let div = document.createElement('div')
 let curseWords = ['cunt','fuck','bitch']
 let curseCounter = 0
+let wordUsed = []
 let obj = {
 
 }
@@ -13,17 +14,29 @@ button.addEventListener("click", () => {
   if(curseWords.includes(word)){
   storeWords(word)
   curseCounter++
+  wordUsed.push(word)
   }
     
   }
+ let wordListArr = []
+  for(let entries of Object.entries(obj)){
+    let word = entries[0];
+    let count = entries[1];
+    wordListArr.push(`${word}(${count})`) 
+    console.log(wordListArr);
+    
+    
+  }
   div.innerHTML = `<div>curse count : ${curseCounter}</div>
-  <div>curse word</div>`
+  <div>curse words use :${wordListArr}</div>`
   document.body.append(div)
-  console.log(curseCounter);
-  console.log(obj);
+  // console.log(curseCounter);
+  // console.log(obj);
   
   
 }) 
+
+
 
 function storeWords(word){
 if(obj[word]){
