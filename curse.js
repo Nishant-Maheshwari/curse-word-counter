1af1aec41cb685880d10b2a3b1a10f32e325c65c
Nishant Maheshwari <nishant2622@gmail.com>
2025-08-20 02:58:47 +0530

    feat: clear input and reset curse count on submit

--- curse.js
+++ curse.js
@@ -2,43 +2,45 @@ let textInput = document.getElementById("textInput");
 let button = document.getElementById("submitBtn"); 
 let div = document.createElement('div')
 let curseWords = ['cunt','fuck','bitch']
-let curseCounter = 0
-let wordUsed = []
-let obj = {
 
-}
+let wordUsed = []
+
 
 button.addEventListener("click", () => {
+  let curseCounter = 0;
+let obj = {};
   let words = textInput.value.toLowerCase().trim().split(/\s+/);
   for(let word of words){
   if(curseWords.includes(word)){
-  storeWords(word)
+  storeWords(word,obj)
   curseCounter++
   wordUsed.push(word)
   }
     
   }
  let wordListArr = []
-  for(let entries of Object.entries(obj)){
-    let word = entries[0];
-    let count = entries[1];
+  for(let [word,count] of Object.entries(obj)){
+    // let word = entries[0];
+    // let count = entries[1];
     wordListArr.push(`${word}(${count})`) 
     console.log(wordListArr);
     
     
   }
   div.innerHTML = `<div>curse count : ${curseCounter}</div>
-  <div>curse words use :${wordListArr}</div>`
+  <div>curse words use :${wordListArr.join("| ")}</div>`
   document.body.append(div)
   // console.log(curseCounter);
   // console.log(obj);
   
+  textInput.value = "";
+ 
   
 }) 
 
 
 
-function storeWords(word){
+function storeWords(word,obj){
 if(obj[word]){
   obj[word]++
 }else{
